3d91db62d1e9aa909485fe8acfea665c7ec2e010
Mariia Kasianova <kasyanovamg@gmail.com>
2020-11-29 20:43:53 +0400

    feat: added a simple dedirect to courses page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My first angular';
+
+  // understanding ordering on lifecycle hooks
+
+  // ngOnChanges(): void {
+  //   console.log("ngOnChanges()");
+  // }
+
+  // ngOnInit(): void {
+  //   console.log("ngOnInit()");
+  // }
+
+  // ngDoCheck(): void {
+  //   console.log("ngDoCheck()");
+  // }
+
+  // ngAfterContentInit(): void {
+  //   console.log("ngAfterContentInit()");
+  // }
+
+  // ngAfterContentChecked(): void {
+  //   console.log("ngAfterContentChecked()");
+  // }
+
+  // ngAfterViewInit(): void {
+  //   console.log("ngAfterViewInit()");
+  // }
+
+  // ngAfterViewChecked(): void {
+  //   console.log("ngAfterViewChecked()");
+  // }
+
+  // ngOnDestroy(): void {
+  //   console.log("ngOnDestroy()");
+  // }
+
 }
